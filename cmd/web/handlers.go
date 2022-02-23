f8802362adc64f8de240eb092624edd549fe273a
Luís Pinto <luispinto23@gmail.com>
2022-02-23 15:20:54 +0000

    feat: added new base and footer templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,21 +14,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	// Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents any
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 
 	if err != nil {
